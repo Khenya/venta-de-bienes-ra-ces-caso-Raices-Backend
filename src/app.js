237935ac880491ee6d1b12e920e94fb1e032294c
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2025-03-01 16:24:56 -0400

    EPICA1-US3-feat: route protection

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
+require('dotenv').config(); 
+
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser'); 
-require('dotenv').config();
+const protectedRoutes = require('./routes/protectedRoutes'); 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', authRoutes);
 
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials: true,
+}));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/protected', protectedRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
