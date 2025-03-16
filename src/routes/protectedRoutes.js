3ddd662791c7a2214e92541179ef6f5e29bc2bf1
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2025-03-16 19:47:55 -0400

    EPICA4-feat: protected routes

--- src/routes/protectedRoutes.js
+++ src/routes/protectedRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middleware/authMiddleware'); 
 const checkPermission = require('../middleware/checkPermission'); 
+const {
+  getAllPropertiesHandler,
+  getPropertiesByUserHandler,
+  getPropertyByIdHandler,
+  getPropertyByStateHandler,
+  getPropertyByPriceHandler,
+  getPropertyByManzanoHandler, 
+  getPropertyByBatchHandler
+} = require('../controllers/propertyController');
 
 router.get('/profile', authenticateToken, (req, res) => {
   res.json({ message: 'Perfil del usuario', user: req.user });
@@ -14,5 +23,19 @@ router.get('/admin', authenticateToken, checkPermission('edit_property'), (req, 
 router.get('/owner', authenticateToken, checkPermission('see_property'), (req, res) => {
   res.json({ message: 'Acceso concedido: Eres un dueño.' });
 });
+
+router.get('/properties', verificarToken, getAllPropertiesHandler);
+
+router.get('/my-properties', verificarToken, getPropertiesByUserHandler);
+
+router.get('/properties/:id', verificarToken, getPropertyByIdHandler);
+
+router.get('/properties/state/:state', verificarToken, getPropertyByStateHandler);
+
+router.get('/properties/price/:price', verificarToken, getPropertyByPriceHandler);
+
+router.get('/properties/manzano/:manzano', verificarToken, getPropertyByManzanoHandler);
+
+router.get('/properties/batch/:batch', verificarToken, getPropertyByBatchHandler);
 
 module.exports = router;
