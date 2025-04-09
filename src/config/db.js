4544fbe2e9105111f1c0a815e772b3c4f54ad352
Khenya-dev <brenda.alvarado@assuresoft.com.bo>
2025-04-09 18:06:35 -0400

    conection with aws

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,9 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST_PG,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 module.exports = pool;
