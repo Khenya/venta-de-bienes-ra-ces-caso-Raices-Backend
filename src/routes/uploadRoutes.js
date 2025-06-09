b5068f5d50c7ae2144aa5f6a15cf3015e5f84d83
Khenya <abrendakhenya@gmail.com>
2025-06-09 16:26:12 -0400

    EPICA - 7 - US- 74 - feat: endpoint for getting maps

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -20,4 +20,24 @@ router.post('/map', upload.single('file'), async (req, res, next) => {
   }
 });
 
+router.get('/maps', async (req, res, next) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = 1; 
+    const skip = (page - 1) * limit;
+
+    const total = await MapModel.countDocuments();
+    const maps = await MapModel.find().sort({ createdAt: -1 }).skip(skip).limit(limit);
+
+    res.json({
+      maps,
+      total,
+      page,
+      totalPages: Math.ceil(total / limit),
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
